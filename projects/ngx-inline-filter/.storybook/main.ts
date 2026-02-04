a5126d44eb92bddc64daeab458ef3fd5e9a3c946
Sebastian Stehle <sebastian@squidex.io>
2026-02-04 22:46:38 +0100

    Fix scrollbar.

--- projects/ngx-inline-filter/.storybook/main.ts
+++ projects/ngx-inline-filter/.storybook/main.ts
@@ -5,15 +5,28 @@ import { resolve } from 'path';
 
 const config: StorybookConfig = {
     stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-    addons: [
-        getAbsolutePath('@storybook/addon-onboarding'),
-        getAbsolutePath('@storybook/addon-docs'),
-    ],
     framework: {
         name: '@analogjs/storybook-angular',
         options: {},
     },
+    async viteFinal(config) {
+        config.css = config.css ?? {};
+        config.css.preprocessorOptions = {
+            ...config.css.preprocessorOptions,
+            scss: {
+                quietDeps: true,
+                silenceDeprecations: [
+                    'import',
+                    'global-builtin',
+                    'color-functions',
+                ],
+            },
+        };
+
+        return config;
+    },
 };
+
 export default config;
 
 function getAbsolutePath(value: string): any {
